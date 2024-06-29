5d4586208dd094982908c7ab0c63303b0297ff44
Piyush_kr_Madhukar <105438331+Piyush5madhukar@users.noreply.github.com>
2024-06-30 02:16:12 +0530

    Update chatpdf1.py

--- chatpdf1.py
+++ chatpdf1.py
@@ -11,7 +11,7 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-os.getenv("GOOGLE_KEY")
+# os.getenv("GOOGLE_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_KEY"))
 
 def get_pdf_text(pdf_docs):
