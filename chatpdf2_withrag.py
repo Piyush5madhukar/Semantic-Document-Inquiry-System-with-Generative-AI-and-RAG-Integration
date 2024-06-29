9f10a144b81999d2826fc0e9612c47f75bf69dbe
Piyush_kr_Madhukar <105438331+Piyush5madhukar@users.noreply.github.com>
2024-06-29 22:35:15 +0530

    Update chatpdf2_withrag.py

--- chatpdf2_withrag.py
+++ chatpdf2_withrag.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+api_key = "API_KEY"
 genai.configure(api_key=api_key)
 
 # Function to extract text from uploaded PDFs
